045c347431c246a857e90db82bec21fef7e4f3f0
devang-ansorbit <devang.patel@ansorbit.com>
2023-07-24 23:30:02 +0530

    code update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   const connect = await mongoose.connect(
-    "mongodb+srv://pateldevang:pateldevang@userdata.nibmr6h.mongodb.net/",
+    process.env.DABASE_URL,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
