fdf28d7857b518a7225119bd22e65f15702f9e1f
devang-ansorbit <devang.patel@ansorbit.com>
2023-07-25 04:03:08 +0530

    extend the json limit

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const dataRoute = require("./Routes/userRoutes");
 
 // cors access
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '500mb'}));
+app.use(express.urlencoded({ extended: true, parameterLimit: 100000, limit: '500mb' }));
 
 //mongodb connection
 connectDB();
